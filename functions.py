1322f3002d70fbfeb29330318f380dc7d71de110
sipneat <jesperson@scu.edu>
2023-08-03 16:25:54 -0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,243 @@
+import os
+import time
+import re
+import pytesseract
+import pdf2image
+import numpy as np
+import cv2 as cv
+from dotenv import load_dotenv
+from decimal import Decimal
+from skimage.metrics import structural_similarity as ssim
+
+load_dotenv()
+
+# Global Variables
+pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_PATH")
+pathToWatch = os.getenv("WATCH_PATH")
+pathForTemps = pathToWatch
+dbPath = os.getenv("DB_PATH")
+signaturePath = os.getenv("SIGNATURE_PATH")
+clients = []
+clientNames = []
+keywords = []
+docNames = []
+cropY = []
+cropX = []
+page = []
+pageNumber = 0
+nameFlag, docFlag, signFlag = False, False, True
+docName = ""
+finalClient = ""
+finalDoc = ""
+finalSign = ""
+finalDate = time.strftime("%Y-%m-%d")
+
+# Function: dbCheck
+# Description: Checks the csv files in the DBs folder and populates the lists
+def dbCheck():
+    global clients, clientNames, keywords, docNames, cropY, cropX
+    with open(dbPath + "\\clients.csv", "r") as f:
+        next(f)
+        for line in f:
+            clients.append(line.split(",")[0])
+            clients[-1] = clients[-1].replace("\n", "")
+            clientNames.append(line.split(",")[1])
+            clientNames[-1] = clientNames[-1].replace("\n", "")
+    f.close()
+    with open(dbPath + "\\docs.csv", "r") as f:
+        next(f)
+        for line in f:
+            keywords.append(line.split(",")[0])
+            keywords[-1] = keywords[-1].replace("\n", "")
+            docNames.append(line.split(",")[1])
+            docNames[-1] = docNames[-1].replace("\n", "")
+            cropY.append(line.split(",")[2])
+            cropY[-1] = cropY[-1].replace("\n", "")
+            cropX.append(line.split(",")[3])
+            cropX[-1] = cropX[-1].replace("\n", "")
+            page.append(line.split(",")[4])
+            page[-1] = page[-1].replace("\n", "")
+    f.close()
+    return
+
+# Function: convert_to_text
+# Description: Converts a pdf file to a text file
+def convert_to_text(page):
+    text = pytesseract.image_to_string(page, lang='eng')
+
+    with open(pathForTemps + '\\test_pdf.txt', 'w') as f:
+        f.write(text)
+    f.close()
+    return
+
+# Function: firstPage
+# Description: Opens the first page of a pdf to find the client and document type
+def firstPage(fpage):
+    global nameFlag, docFlag, finalClient, finalDoc, finalDate, page, pageNumber, finalSign, signFlag
+
+    convert_to_text(fpage)
+    with open(pathForTemps + "\\test_pdf.txt", "r") as f:
+        for x in clients:
+            if nameFlag:
+                break
+            lineCount = 0
+            f.seek(0)
+            line = f.readline(1)
+            for line in f:
+                if lineCount >= 40:
+                    break
+                lineCount += 1
+                if x in line:
+                    print("Client Found")
+                    nameFlag = True
+                    finalClient = clientNames[clients.index(x)]
+                    break
+        if not nameFlag:
+            print("Client Not Found")
+            f.close()
+            return
+        
+        for x in keywords:
+            if docFlag:
+                break
+            lineCount = 0
+            f.seek(0)
+            line = f.readline(1)
+            for line in f:
+                if lineCount >= 50:
+                    break
+                lineCount += 1
+                if x in line:
+                    print("Doc Found")
+                    docFlag = True
+                    finalDoc = docNames[keywords.index(x)]
+                    pageNumber = int(page[keywords.index(x)])
+                    break
+        if not docFlag:
+            print("Doc Not Found")
+            f.close()
+            return
+    f.close()
+
+    ssimScore = 0
+    img = np.array(fpage)
+
+    img = cv.resize(img, (1000, 1000))
+    y = 55
+    x = 650
+    h = 225
+    w = 300
+    crop_img = img[y:y+h, x:x+w]
+
+    i = cv.cvtColor(cv.imread(signaturePath + '\\stamp.jpg'),cv.COLOR_BGR2RGB)
+    bad_image = cv.resize(cv.cvtColor(i.copy(),cv.COLOR_BGR2GRAY),(300,100))
+    original_image = cv.resize(cv.cvtColor(crop_img.copy(),cv.COLOR_BGR2GRAY),(300,100))
+    ssimScore = ssim(original_image,bad_image)
+    print("Stamp Score: " + str(ssim(original_image,bad_image)))
+
+    if ssimScore <= Decimal(0.8):
+        print("Stamp is a match!")
+        finalSign = "(F)"
+        signFlag = False
+    else:
+        print("No stamp")
+    return
+
+# Function: signedPage
+# Description: Opens the signature page of a pdf to find the date and if a signature is present
+def signedPage(spage):
+    global finalSign, finalDoc, finalDate, cropY, cropX
+    
+    convert_to_text(spage)
+    with open(pathForTemps + "\\test_pdf.txt", "r") as f:
+        global dateFlag
+        dateFlag = False
+        line = f.readline(1)
+        pattern = r"[\d]{1,2}\/[\d]{1,2}\/[\d]{2,4}" + "|" + r"[\d]{1,2}\-[\d]{1,2}\-[\d]{2,4}" + "|" + r"[\d]{1,2}\.[\d]{1,2}\.[\d]{2,4}"
+        for line in f:
+            if re.search(pattern, line):
+                print("Date Found")
+                temp = re.search(pattern, line).group()
+                temp = temp.split("/")
+                if len(temp) == 1:
+                    temp = temp[0].split("-")
+                if len(temp) == 1:
+                    temp = temp[0].split(".")
+                if len(temp[2]) == 2:
+                    temp[2] = "20" + temp[2]
+                if int(temp[2]) <= (int(time.strftime("%Y")) - 1):
+                    oldFlag = True
+                if oldFlag:
+                    continue
+                if len(temp[0]) == 1:
+                    temp[0] = "0" + temp[0]
+                if len(temp[1]) == 1:
+                    temp[1] = "0" + temp[1]
+                finalDate = temp[2] + "-" + temp[0] + "-" + temp[1]
+                dateFlag = True
+                break
+        if not dateFlag:
+            print("Date Not Found")
+        f.close()
+    
+    if signFlag:
+        ssimScore = 0
+        img = np.array(spage)
+
+        img = cv.resize(img, (1000, 1000))
+        y = int(cropY[docNames.index(finalDoc)])
+        x = int(cropX[docNames.index(finalDoc)])
+        h = 50
+        w = 300
+        crop_img = img[y:y+h, x:x+w]
+
+        i = cv.cvtColor(cv.imread(signaturePath + '\\' + finalDoc + '.jpg'),cv.COLOR_BGR2RGB)
+        bad_image = cv.resize(cv.cvtColor(i.copy(),cv.COLOR_BGR2GRAY),(300,100))
+        original_image = cv.resize(cv.cvtColor(crop_img.copy(),cv.COLOR_BGR2GRAY),(300,100))
+        ssimScore = ssim(original_image,bad_image)
+        print("Signature Score: " + str(ssim(original_image,bad_image)))
+
+        if ssimScore <= Decimal(0.8):
+            print("Signature is a match!")
+            finalSign = "(S)"
+        else:
+            print("No signature match")
+    return
+
+def destroy():
+    global clients, clientNames, keywords, docNames, cropY, cropX, page, nameFlag, docFlag, docName, finalClient, finalDoc, finalSign, finalDate, pageNumber, signFlag
+    clients, clientNames, keywords, docNames, cropY, cropX, page = [], [], [], [], [], [], []
+    pageNumber = 0
+    nameFlag, docFlag, signFlag = False, False, True
+    docName = ""
+    finalClient = ""
+    finalDoc = ""
+    finalSign = ""
+    finalDate = time.strftime("%Y-%m-%d")
+    return
+
+# Function: file_rename
+# Description: Renames a file based on the name of client and type of document of the file
+# Format: YYYY-MM-DD CLIENT_NAME DOCUMENT_TYPE (S)
+def file_rename(newFile):
+    global docName, nameFlag, docFlag, finalClient, finalDoc, finalSign, finalDate, pageNumber
+    dbCheck()
+    pages = pdf2image.convert_from_path(pathToWatch + "\\" + newFile, 500)
+
+    firstPage(pages[0])
+    if nameFlag and docFlag:
+        signedPage(pages[pageNumber])
+
+    docName = finalDate + " " + finalClient + finalDoc + finalSign
+    docName = docName.strip()
+    print(docName)
+    os.remove(pathForTemps + "\\test_pdf.txt")
+    if nameFlag and docFlag:
+        src = pathToWatch + '\\' + newFile
+        des = pathToWatch + '\\' + docName + '.pdf'
+        os.rename(src, des)
+        destroy()
+        return
+    else:
+        print("File not renamed")
+        return
---
+++ script.py
@@ -0,0 +1,47 @@
+import os
+import time
+from functions import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Global Variables
+newFile = []
+pathToWatch = os.getenv("WATCH_PATH")
+pathForTemps = pathToWatch
+
+# Function: main
+# Description: Watches a directory for changes and calls file_rename under certain conditions
+def main():
+    print("Watching " + pathToWatch + " for changes")
+    old = os.listdir(pathToWatch)
+    print(old)
+    while 1:
+        new = os.listdir(pathToWatch)
+        if len(new) != len(old):
+            global newFile
+            newFile = list(set(new) - set(old))
+            if len(newFile) != 1:
+                print("File removed")
+                old = new
+                time.sleep(2)
+                continue
+            print(newFile[0])
+            extension = os.path.splitext(pathToWatch + "\\" + newFile[0])[1]
+            if extension == ".pdf":
+                start_time = time.time()
+                file_rename(newFile[0])
+                print("--- %s seconds ---" % (time.time() - start_time))
+                old = new
+                time.sleep(2)
+                continue
+            else:
+                print("Not a pdf")
+                old = new
+                time.sleep(2)
+                continue
+        else:
+            print("No changes")
+            time.sleep(5)
+
+main()
