22dc9a78944d9a3afa669a7dbf26297844519291
sipneat <jesperson@scu.edu>
2023-08-10 13:20:53 -0700

    Added queue for new files

--- script.py
+++ script.py
@@ -1,19 +1,34 @@
 import os
 import time
+import threading
 from functions import *
 from dotenv import load_dotenv
+from collections import deque
 
 load_dotenv()
 
 # Global Variables
 newFile = []
+queue = deque()
 pathToWatch = os.getenv("WATCH_PATH")
-pathForTemps = pathToWatch
+
+
+# Function: fileQueue
+# Description: Handles file queue
+def fileQueue():
+    global queue
+    while 1:
+        if len(queue) > 0:
+            start_time = time.time()
+            fileRename(queue.popleft())
+            print("--- %s seconds ---" % (time.time() - start_time))
+        else:
+            time.sleep(5)
 
 
 # Function: main
 # Description: Watches a directory for changes and calls file_rename under certain conditions
-def main():
+def fileWatch():
     print("Watching " + pathToWatch + " for changes")
     old = os.listdir(pathToWatch)
     print(old)
@@ -32,9 +47,7 @@ def main():
             print(newFile[0])
             extension = os.path.splitext(pathToWatch + "\\" + newFile[0])[1]
             if extension == ".pdf":
-                start_time = time.time()
-                file_rename(newFile[0])
-                print("--- %s seconds ---" % (time.time() - start_time))
+                queue.append(newFile[0])
                 new = os.listdir(pathToWatch)
                 old = new
                 time.sleep(2)
@@ -55,4 +68,7 @@ def main():
                 time.sleep(5)
 
 
-main()
+t1 = threading.Thread(target=fileWatch)
+t2 = threading.Thread(target=fileQueue)
+t1.start()
+t2.start()
