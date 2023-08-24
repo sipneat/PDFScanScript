e886bfae3eb67fbe55c4093fe806c958e8635f4f
sipneat <jesperson@scu.edu>
2023-08-24 12:39:54 -0700

    Added comments

--- functions.py
+++ functions.py
@@ -38,10 +38,11 @@ finalDate = time.strftime("%Y-%m-%d")
 
 
 # Function: dbCheck
-# Description: Checks the csv files in the DBs folder and populates the lists
+# Description: Checks the csv files in the DBs folder and populates the global variables for use
 def dbCheck():
     global clientPath, clients, clientNames, keywords, docNames, signY, signX, signPage, stampY, stampX
 
+    # Clients are pulled from the client directory instead of a csv file. This ensures that the client names are always up to date
     clientFileNames = os.listdir(clientPath)
     for x in clientFileNames:
         if os.path.isfile(clientPath + "\\" + x):
@@ -51,6 +52,8 @@ def dbCheck():
         firstInitial = firstInitial.strip()
         firstInitial = firstInitial[0]
         clientNames.append(clients[-1] + " " + firstInitial)
+
+    # docs.csv contains the keywords, proper file names, and signature/stamp crop coordinates for use by cv.
     with open(dbPath + "\\docs.csv", "r") as f:
         next(f)
         for line in f:
@@ -84,33 +87,35 @@ def convert_to_text(page):
 
 
 # Function: firstPage
-# Description: Opens the first page of a pdf to find the client and document type
+# Description: Opens the first page of a pdf to find the client, document type, and stamp if present
 def firstPage(fpage):
     global nameFlag, docFlag, finalClient, finalDoc, finalDate, signPage, pageNumber, finalSign, signFlag, stampY, stampX
 
     convert_to_text(fpage)
     with open(pathForTemps + "\\test_pdf.txt", "r") as f:
         for x in clients:
-            if nameFlag:
+            if nameFlag:  # If client is found, break out of the loop
                 break
             lineCount = 0
             f.seek(0)
             for line in f:
-                if lineCount >= 40:
+                if lineCount >= 40:  # Only search the first 40 lines of the pdf
                     break
                 lineCount += 1
-                line = line.upper()
+                line = (
+                    line.upper()
+                )  # Convert the line to uppercase to make the search case insensitive
                 if x in line:
                     print("Client Found")
                     nameFlag = True
                     finalClient = clientNames[clients.index(x)]
                     break
-        if not nameFlag:
+        if not nameFlag:  # If client is not found, the file cannot be renamed
             print("Client Not Found")
             f.close()
             return
 
-        for x in keywords:
+        for x in keywords:  # Same process as above but for document type
             if docFlag:
                 break
             lineCount = 0
@@ -132,20 +137,22 @@ def firstPage(fpage):
     f.close()
 
     ssimScore = 0
-    img = np.array(fpage)
+    img = np.array(fpage)  # Convert the pdf page to a numpy array
 
-    img = cv.resize(img, (1000, 1000))
-    y = int(stampY[docNames.index(finalDoc)])
+    img = cv.resize(img, (1000, 1000))  # Resize the image
+    y = int(
+        stampY[docNames.index(finalDoc)]
+    )  # Get the crop coordinates from the csv file
     x = int(stampX[docNames.index(finalDoc)])
     h = 225
     w = 300
-    crop_img = img[y : y + h, x : x + w]
+    crop_img = img[y : y + h, x : x + w]  # Crop the image
 
     try:
         i = cv.cvtColor(
             cv.imread(stampPath + "\\" + keywords[docNames.index(finalDoc)] + ".jpg"),
             cv.COLOR_BGR2RGB,
-        )
+        )  # Open the known bad stamp image
     except:
         print("ERROR: Stamp file is incorrect, maybe name is wrong?")
         destroy()
@@ -155,10 +162,14 @@ def firstPage(fpage):
     original_image = cv.resize(
         cv.cvtColor(crop_img.copy(), cv.COLOR_BGR2GRAY), (300, 225)
     )
-    ssimScore = ssim(original_image, bad_image)
+    ssimScore = ssim(
+        original_image, bad_image
+    )  # Compare the cropped image to the known bad stamp image using ssim
     print("Stamp Score: " + str(ssim(original_image, bad_image)))
 
-    if ssimScore <= Decimal(0.8):
+    if ssimScore <= Decimal(
+        0.8
+    ):  # If the ssim score is below 0.8, the stamp is a match
         print("Stamp is a match!")
         finalSign = "(F)"
         signFlag = False
@@ -175,7 +186,7 @@ def signedPage(spage):
     convert_to_text(spage)
     with open(pathForTemps + "\\test_pdf.txt", "r") as f:
         global dateFlag
-        for _ in zip(range(35), f):
+        for _ in zip(range(35), f):  # Skip the first 35 lines of the pdf
             pass
         dateFlag = False
         pattern = (
@@ -184,7 +195,7 @@ def signedPage(spage):
             + r"[\d]{1,2}\-[\d]{1,2}\-[\d]{2,4}"
             + "|"
             + r"[\d]{1,2}\.[\d]{1,2}\.[\d]{2,4}"
-        )
+        )  # Regex pattern to find a date
         for line in f:
             if re.search(pattern, line):
                 print("Date Found")
@@ -197,9 +208,13 @@ def signedPage(spage):
                     temp = temp[0].split(".")
                 if len(temp[2]) == 2:
                     temp[2] = "20" + temp[2]
-                if int(temp[2] + temp[0] + temp[1]) > int(finalDate.replace("-", "")):
+                if int(temp[2] + temp[0] + temp[1]) > int(
+                    finalDate.replace("-", "")
+                ):  # If the date is in the future, it is invalid
                     newFlag = True
-                if int(temp[2]) <= (int(time.strftime("%Y")) - 1):
+                if int(temp[2]) <= (
+                    int(time.strftime("%Y")) - 1
+                ):  # If the date is too old, it is invalid
                     oldFlag = True
                 if oldFlag:
                     print("Date is too old: " + temp[2])
@@ -211,7 +226,9 @@ def signedPage(spage):
                     temp[0] = "0" + temp[0]
                 if len(temp[1]) == 1:
                     temp[1] = "0" + temp[1]
-                finalDate = temp[2] + "-" + temp[0] + "-" + temp[1]
+                finalDate = (
+                    temp[2] + "-" + temp[0] + "-" + temp[1]
+                )  # Format the date in YYYY-MM-DD
                 dateFlag = True
                 break
         if not dateFlag:
@@ -220,14 +237,16 @@ def signedPage(spage):
 
     if signFlag:
         ssimScore = 0
-        img = np.array(spage)
+        img = np.array(spage)  # Convert the pdf page to a numpy array
 
-        img = cv.resize(img, (1000, 1000))
-        y = int(signY[docNames.index(finalDoc)])
+        img = cv.resize(img, (1000, 1000))  # Resize the image
+        y = int(
+            signY[docNames.index(finalDoc)]
+        )  # Get the crop coordinates from the csv file
         x = int(signX[docNames.index(finalDoc)])
         h = 50
         w = 300
-        crop_img = img[y : y + h, x : x + w]
+        crop_img = img[y : y + h, x : x + w]  # Crop the image
 
         try:
             i = cv.cvtColor(
@@ -235,7 +254,7 @@ def signedPage(spage):
                     signaturePath + "\\" + keywords[docNames.index(finalDoc)] + ".jpg"
                 ),
                 cv.COLOR_BGR2RGB,
-            )
+            )  # Open the known bad signature image
         except:
             print("ERROR: Signature file is incorrect, maybe name is wrong?")
             destroy()
@@ -245,10 +264,14 @@ def signedPage(spage):
         original_image = cv.resize(
             cv.cvtColor(crop_img.copy(), cv.COLOR_BGR2GRAY), (300, 50)
         )
-        ssimScore = ssim(original_image, bad_image)
+        ssimScore = ssim(
+            original_image, bad_image
+        )  # Compare the cropped image to the known bad signature image using ssim
         print("Signature Score: " + str(ssim(original_image, bad_image)))
 
-        if ssimScore <= Decimal(0.8):
+        if ssimScore <= Decimal(
+            0.8
+        ):  # If the ssim score is below 0.8, the signature is a match
             print("Signature is a match!")
             finalSign = "(S)"
         else:
@@ -284,12 +307,14 @@ def destroy():
 
 # Function: file_rename
 # Description: Renames a file based on the name of client and type of document of the file
-# Format: YYYY-MM-DD CLIENT_NAME DOCUMENT_TYPE (S/F)
+# Format: YYYY-MM-DD CLIENT_NAME DOCUMENT_TYPE (S/F/D)
 def fileRename(newFile):
     global docName, nameFlag, docFlag, finalClient, finalDoc, finalSign, finalDate, pageNumber
     dbCheck()
     try:
-        pages = pdf2image.convert_from_path(pathToWatch + "\\" + newFile, 500)
+        pages = pdf2image.convert_from_path(
+            pathToWatch + "\\" + newFile, 500
+        )  # Convert the pdf to images
     except:
         print("ERROR: File is corrupted")
         src = pathToWatch + "\\" + newFile
@@ -299,28 +324,36 @@ def fileRename(newFile):
         return
 
     firstPage(pages[0])
-    if nameFlag and docFlag:
+    if (
+        nameFlag and docFlag
+    ):  # If the client and document type are not found on the first page, the signature page is not needed
         signedPage(pages[pageNumber])
 
-    docName = finalDate + " " + finalClient + " " + finalDoc + " " + finalSign
-    docName = docName.strip()
+    docName = (
+        finalDate + " " + finalClient + " " + finalDoc + " " + finalSign
+    )  # Combine the variables into a string
+    docName = docName.strip()  # Remove any extra spaces
     print(docName)
-    os.remove(pathForTemps + "\\test_pdf.txt")
-    if nameFlag and docFlag:
+    os.remove(pathForTemps + "\\test_pdf.txt")  # Delete the temporary text file
+    if (
+        nameFlag and docFlag
+    ):  # If the client and document type are found, rename the file
         try:
             src = pathToWatch + "\\" + newFile
             des = pathToWatch + "\\" + docName + ".pdf"
-            os.rename(src, des)
+            os.rename(src, des)  # Rename the file
             destroy()
             return
         except:
             print("ERROR: File already exists")
             src = pathToWatch + "\\" + newFile
             des = src.replace(".pdf", " (EXISTS).pdf")
-            os.rename(src, des)
+            os.rename(
+                src, des
+            )  # If the file already exists, rename the file with (EXISTS) appended to the end
             destroy()
             return
-    else:
+    else:  # If the client and document type are not found, the file cannot be renamed
         print("File not renamed")
         destroy()
         return
--- script.py
+++ script.py
@@ -18,10 +18,12 @@ pathToWatch = os.getenv("WATCH_PATH")
 def fileQueue():
     global queue
     while 1:
-        if len(queue) > 0:
+        if len(queue) > 0:  # If queue is not empty
             start_time = time.time()
-            fileRename(queue.popleft())
-            print("--- %s seconds ---" % (time.time() - start_time))
+            fileRename(queue.popleft())  # Rename file
+            print(
+                "--- %s seconds ---" % (time.time() - start_time)
+            )  # Print time to rename file
         else:
             time.sleep(5)
 
@@ -30,45 +32,49 @@ def fileQueue():
 # Description: Watches a directory for changes and calls file_rename under certain conditions
 def fileWatch():
     print("Watching " + pathToWatch + " for changes")
-    old = os.listdir(pathToWatch)
+    old = os.listdir(pathToWatch)  # Get original list of files in directory
     print(old)
     new = old
     count = 0
     while 1:
-        new = os.listdir(pathToWatch)
-        if len(new) != len(old):
+        new = os.listdir(pathToWatch)  # Get list of files in directory
+        if len(new) != len(old):  # If number of files has changed
             global newFile
-            newFile = list(set(new) - set(old))
-            if len(newFile) != 1:
+            newFile = list(set(new) - set(old))  # Get new file
+            if (
+                len(newFile) != 1
+            ):  # If more than one file has been added or files have been removed, do nothing
                 print("File removed")
                 new = os.listdir(pathToWatch)
                 old = new
                 time.sleep(2)
                 continue
             temp = newFile[0].split(" ")
-            if len(temp) != 1:
+            if len(temp) != 1:  # If file has already been renamed, do nothing
                 print("File has already been renamed")
                 new = os.listdir(pathToWatch)
                 old = new
                 time.sleep(2)
                 continue
             print(newFile[0])
-            extension = os.path.splitext(pathToWatch + "\\" + newFile[0])[1]
-            if extension == ".pdf":
+            extension = os.path.splitext(pathToWatch + "\\" + newFile[0])[
+                1
+            ]  # Get file extension
+            if extension == ".pdf":  # If file is a pdf, add to queue
                 queue.append(newFile[0])
                 new = os.listdir(pathToWatch)
                 old = new
                 time.sleep(2)
                 continue
-            else:
+            else:  # If file is not a pdf, do nothing
                 print("Not a pdf")
                 new = os.listdir(pathToWatch)
                 old = new
                 time.sleep(2)
                 continue
-        else:
+        else:  # If number of files has not changed, do nothing
             count += 1
-            if count > 720:
+            if count > 720:  # If no changes for 1 hour, print "No Changes"
                 count = 0
                 print("No Changes")
                 new = os.listdir(pathToWatch)
@@ -80,7 +86,7 @@ def fileWatch():
                 time.sleep(5)
 
 
-t1 = threading.Thread(target=fileWatch)
-t2 = threading.Thread(target=fileQueue)
+t1 = threading.Thread(target=fileWatch)  # Create thread for fileWatch
+t2 = threading.Thread(target=fileQueue)  # Create thread for fileQueue
 t1.start()
 t2.start()
